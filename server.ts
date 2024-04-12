0fdfecd67416f21c1398cf8dc77d9efaba2bb948
shadab <shadab.uok@gmail.com>
2024-04-12 09:53:10 +0500

    [ebook-backend] feat: setup express

--- server.ts
+++ server.ts
@@ -1,3 +1,7 @@
-function add() {
-  console.log('ffdf')
-}
+import app from './src/app'
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`server is runnning on port : ${port}`)
+})
