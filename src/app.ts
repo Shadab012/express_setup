305f809e9119547f8143659d5413a687e1105724
shadab <shadab.uok@gmail.com>
2024-04-13 10:08:20 +0500

    [ebook-backend] feat: add create user route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { globalErrorHandler } from './middlewares/globalErrorHandlers'
+import userRouter from './features/users/userRouter'
 
 const app = express()
 
@@ -7,5 +8,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'initial setup is done' })
 })
 
+app.use('/api/users', userRouter)
 app.use(globalErrorHandler)
 export default app
